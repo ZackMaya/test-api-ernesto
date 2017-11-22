03d294bbfc89c91230e9a2b1f3eff7de93715225
ZackMaya <ingsoftnetozack@outlook.com>
2017-11-22 09:34:45 -0600

    make it better

--- src/main/java/mx/edu/uacm/rest/controller/SaludoController.java
+++ src/main/java/mx/edu/uacm/rest/controller/SaludoController.java
@@ -2,6 +2,7 @@ package mx.edu.uacm.rest.controller;
 
 import java.util.concurrent.atomic.AtomicLong;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,7 +21,8 @@ public class SaludoController {
 	//mapeo para los métodos get, post, put, delete (todas las operaciones http)
 	//get ---> /saludame
 	//post ----> /saludame...
-	@RequestMapping(value="/saludame")
+	@GetMapping("/saludame")
+	//@RequestMapping(value="/saludame")
 	@ApiOperation(value="obtiene una cadena de saludo", response=Saludo.class)
 	public Saludo saludar(@RequestParam (name="zack",defaultValue="Hola mundo cruel")String nombre) {
 		long incrementado = contador.getAndIncrement();
