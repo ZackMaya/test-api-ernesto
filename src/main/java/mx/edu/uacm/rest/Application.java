759249767f0a657a45331c3f30ffab87e719b0ea
ZackMaya <ingsoftnetozack@outlook.com>
2017-11-22 09:15:33 -0600

    make it better

---
+++ src/main/java/mx/edu/uacm/rest/Application.java
@@ -0,0 +1,16 @@
+package mx.edu.uacm.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(Application.class, args);
+	}
+
+	
+	
+}
---
+++ src/main/java/mx/edu/uacm/rest/controller/SaludoController.java
@@ -0,0 +1,31 @@
+package mx.edu.uacm.rest.controller;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import io.swagger.annotations.ApiOperation;
+import mx.edu.uacm.rest.domain.Saludo;
+
+@RestController
+public class SaludoController {
+
+	private static final String plantilla= "Hola %s";
+	
+	//Valor atomico, con un autoincrementador
+	private final AtomicLong contador = new AtomicLong();
+	
+	//mapeo para los métodos get, post, put, delete (todas las operaciones http)
+	//get ---> /saludame
+	//post ----> /saludame...
+	@RequestMapping(value="/saludame")
+	@ApiOperation(value="obtiene una cadena de saludo", response=Saludo.class)
+	public Saludo saludar(@RequestParam (name="zack",defaultValue="Hola mundo cruel")String nombre) {
+		long incrementado = contador.getAndIncrement();
+		String formato = String.format(plantilla, nombre);
+		return new Saludo(incrementado, formato);
+	}
+	
+}
